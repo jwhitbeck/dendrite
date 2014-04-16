57dbfdda38312d53148897398e06ec7ee443fbf8
John Whitbeck <john@whitbeck.fr>
2014-04-16 00:10:50 -0700

    Add encodings helpers

--- src/dendrite/encodings.clj
+++ src/dendrite/encodings.clj
@@ -1,13 +1,20 @@
 (ns dendrite.encodings
   (:import [dendrite.java
-            BooleanEncoder BooleanDecoder
-            Int32Encoder Int32Decoder
-            Int64Encoder Int64Decoder
-            FloatEncoder FloatDecoder
-            DoubleEncoder DoubleDecoder
+            BooleanEncoder BooleanDecoder BooleanPackedEncoder BooleanPackedDecoder
+            Int32Encoder Int32Decoder Int32PlainEncoder Int32PlainDecoder
+            Int32PackedDeltaEncoder Int32PackedDeltaDecoder
+            Int32FixedBitWidthPackedRunLengthEncoder Int32FixedBitWidthPackedRunLengthDecoder
+            Int32PackedRunLengthEncoder Int32PackedRunLengthDecoder
+            Int64Encoder Int64Decoder Int64PlainEncoder Int64PlainDecoder
+            Int64PackedDeltaEncoder Int64PackedDeltaDecoder
+            FloatEncoder FloatDecoder FloatPlainEncoder FloatPlainDecoder
+            DoubleEncoder DoubleDecoder DoublePlainEncoder DoublePlainDecoder
             FixedLengthByteArrayEncoder FixedLengthByteArrayDecoder
-            ByteArrayEncoder ByteArrayDecoder
-            BooleanPackedEncoder BooleanPackedDecoder]))
+            FixedLengthByteArrayPlainEncoder FixedLengthByteArrayPlainDecoder
+            ByteArrayEncoder ByteArrayDecoder ByteArrayPlainEncoder ByteArrayPlainDecoder
+            ByteArrayIncrementalEncoder ByteArrayIncrementalDecoder
+            ByteArrayDeltaLengthEncoder ByteArrayDeltaLengthDecoder
+            ByteArrayReader]))
 
 (set! *warn-on-reflection* true)
 
@@ -53,3 +60,64 @@
 
 (defn encode-values [encoder values]
   (reduce encode encoder values))
+
+(def ^:private valid-encodings-for-types
+  {:boolean #{:plain}
+   :int32 #{:plain :packed-run-length :delta}
+   :int64 #{:plain :delta}
+   :float #{:plain}
+   :double #{:plain}
+   :byte-array #{:plain :incremental :delta-length}
+   :fixed-length-byte-array #{:plain}})
+
+(defn valid-value-type? [value-type]
+  (-> valid-encodings-for-types keys set (contains? value-type)))
+
+(defn valid-encoding-for-type? [value-type encoding]
+  (contains? (get valid-encodings-for-types value-type) encoding))
+
+(defn list-encodings-for-type [value-type] (get valid-encodings-for-types value-type))
+
+(defn decoder-ctor [value-type encoding]
+  (case value-type
+    :boolean #(BooleanPackedDecoder. %)
+    :int32 (case encoding
+             :plain #(Int32PlainDecoder. %)
+             :packed-run-length #(Int32PackedRunLengthDecoder. %)
+             :delta #(Int32PackedDeltaDecoder. %))
+    :int64 (case encoding
+             :plain #(Int64PlainDecoder. %)
+             :delta #(Int64PackedDeltaDecoder. %))
+    :float #(FloatPlainDecoder. %)
+    :double #(DoublePlainDecoder. %)
+    :byte-array (case encoding
+                  :plain #(ByteArrayPlainDecoder. %)
+                  :incremental #(ByteArrayIncrementalDecoder. %)
+                  :delta-length #(ByteArrayDeltaLengthDecoder. %))
+    :fixed-length-byte-array #(FixedLengthByteArrayPlainDecoder. %)))
+
+(defn encoder [value-type encoding]
+  (case value-type
+    :boolean (BooleanPackedEncoder.)
+    :int32 (case encoding
+             :plain (Int32PlainEncoder.)
+             :packed-run-length (Int32PackedRunLengthEncoder.)
+             :delta (Int32PackedDeltaEncoder.))
+    :int64 (case encoding
+             :plain (Int64PlainEncoder.)
+             :delta (Int64PackedDeltaEncoder.))
+    :float (FloatPlainEncoder.)
+    :double (DoublePlainEncoder.)
+    :byte-array (case encoding
+                  :plain (ByteArrayPlainEncoder.)
+                  :incremental (ByteArrayIncrementalEncoder.)
+                  :delta-length (ByteArrayDeltaLengthEncoder.))
+    :fixed-length-byte-array (FixedLengthByteArrayPlainEncoder.)))
+
+(defn- packed-bit-width [n] (- 32 (Integer/numberOfLeadingZeros n)))
+
+(defn levels-encoder [max-definition-level]
+  (Int32FixedBitWidthPackedRunLengthEncoder. (packed-bit-width max-definition-level)))
+
+(defn levels-decoder [^ByteArrayReader byte-array-reader max-definition-level]
+  (Int32FixedBitWidthPackedRunLengthDecoder. byte-array-reader (packed-bit-width max-definition-level)))
