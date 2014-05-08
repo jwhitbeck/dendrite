e791eb5e683d986889acb1fe7d9c1a72b2ab797a
John Whitbeck <john@whitbeck.fr>
2014-05-08 15:10:37 -0700

    More page tests

--- test/dendrite/page_test.clj
+++ test/dendrite/page_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [dendrite.core :refer [wrap-value]]
             [dendrite.page :refer :all]
-            [dendrite.test-helpers :refer [get-byte-array-reader]]))
+            [dendrite.test-helpers :refer [get-byte-array-reader]])
+  (:import [dendrite.java ByteArrayWriter]))
 
 (defn- rand-wrapped-value [schema-depth]
   (let [definition-level (rand-int (inc schema-depth))
@@ -42,7 +43,7 @@
         page-reader-ctor #(dictionary-page-reader % value-type encoding compression-type)]
     (-> page-writer (write-all input-values) get-byte-array-reader page-reader-ctor read-page)))
 
-(deftest write-read-page
+(deftest data-page
   (testing "Write/read a data page works"
     (testing "uncompressed"
       (let [max-definition-level 3
@@ -79,7 +80,32 @@
             input-values (repeatedly 1000 #(rand-required-top-level-wrapped-value))
             output-values (write-read-single-data-page max-definition-level true
                                                        :int32 :plain :none input-values)]
-        (is (= output-values input-values)))))
+        (is (= output-values input-values))))
+    (testing "empty page"
+      (let [max-definition-level 1
+            input-values []
+            output-values (write-read-single-data-page max-definition-level true
+                                                       :int32 :plain :none input-values)]
+        (is (= output-values input-values))))
+    (testing "repeatable writes"
+      (let [max-definition-level 3
+            input-values (repeatedly 1000 #(rand-wrapped-value max-definition-level))
+            page-writer (-> (data-page-writer max-definition-level false :int32 :plain :none)
+                            (write-all input-values))
+            baw1 (doto (ByteArrayWriter. 10) (.write page-writer))
+            baw2 (doto (ByteArrayWriter. 10) (.write page-writer))]
+        (is (= (-> baw1 .buffer seq) (-> baw2 .buffer seq)))))
+    (testing "repeatable reads"
+      (let [max-definition-level 3
+            input-values (repeatedly 1000 #(rand-wrapped-value max-definition-level))
+            page-writer (-> (data-page-writer max-definition-level false :int32 :plain :none)
+                            (write-all input-values))
+            page-reader (-> page-writer
+                            get-byte-array-reader
+                            (data-page-reader max-definition-level :int32 :plain :none))]
+        (is (= (read-page page-reader) (read-page page-reader)))))))
+
+(deftest dictionary-page
   (testing "Write/read a dictionary page works"
     (testing "uncompressed"
       (let [input-values (repeatedly 1000 #(rand-int 10000))
@@ -88,7 +114,28 @@
     (testing "compressed"
       (let [input-values (repeatedly 1000 #(rand-int 10000))
             output-values (write-read-single-dictionary-page :int32 :plain :lz4 input-values)]
-        (is (= output-values input-values)))))
+        (is (= output-values input-values))))
+    (testing "empty page"
+      (let [input-values []
+            output-values (write-read-single-dictionary-page :int32 :plain :none input-values)]
+        (is (= output-values input-values))))
+    (testing "repeatable writes"
+      (let [input-values (repeatedly 1000 #(rand-int 10000))
+            page-writer (-> (dictionary-page-writer :int32 :plain :none)
+                            (write-all input-values))
+            baw1 (doto (ByteArrayWriter. 10) (.write page-writer))
+            baw2 (doto (ByteArrayWriter. 10) (.write page-writer))]
+        (is (= (-> baw1 .buffer seq) (-> baw2 .buffer seq)))))
+    (testing "repeatable reads"
+      (let [input-values (repeatedly 1000 #(rand-int 10000))
+            page-writer (-> (dictionary-page-writer :int32 :plain :none)
+                            (write-all input-values))
+            page-reader (-> page-writer
+                            get-byte-array-reader
+                            (dictionary-page-reader :int32 :plain :none))]
+        (is (= (read-page page-reader) (read-page page-reader)))))))
+
+(deftest incompatible-pages
   (testing "Read incompatible page types throws an exception"
     (let [data-bar (-> (data-page-writer 1 false :int32 :plain :none)
                        (write-all (repeatedly 100 #(rand-wrapped-value 1)))
