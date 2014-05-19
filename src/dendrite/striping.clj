c74fba3727c828a8a5bacbd8550c7f3bef93cc17
John Whitbeck <john@whitbeck.fr>
2014-05-18 18:32:52 -0700

    Fix invalid record exceptions

--- src/dendrite/striping.clj
+++ src/dendrite/striping.clj
@@ -11,7 +11,7 @@
 (defn- format-parents [parents] (format "[%s]" (string/join " " parents)))
 
 (defmulti recursively-stripe-record
-  (fn [striped-record record schema parents coercion-fns repetition-level definition-level]
+  (fn [striped-record record schema parents nil-parent? coercion-fns repetition-level definition-level]
     (case (:repetition schema)
       (:vector :list :set) :seq
       :map :map
@@ -20,8 +20,8 @@
         :atomic))))
 
 (defmethod recursively-stripe-record :atomic
-  [striped-record record schema parents coercion-fns repetition-level definition-level]
-  #_(if (and (= :required (:repetition schema)) (nil? record))
+  [striped-record record schema parents nil-parent? coercion-fns repetition-level definition-level]
+  (when (and (= :required (:repetition schema)) (nil? record) (not nil-parent?))
     (throw (IllegalArgumentException. (format "Required field %s is missing" (format-parents parents)))))
   (let [value-type (:value schema)
         value (when record
@@ -36,48 +36,54 @@
     (update-in striped-record [(:column-index value-type)] #(conj (or % [] leveled-value) value-with-level))))
 
 (defmethod recursively-stripe-record :optional
-  [striped-record record schema parents coercion-fns repetition-level definition-level]
+  [striped-record record schema parents nil-parent? coercion-fns repetition-level definition-level]
   (let [definition-level (if (empty? record) definition-level (inc definition-level))]
     (reduce (fn [striped-record field]
               (let [v (get record (:name field))]
-                (recursively-stripe-record striped-record v field (conj parents (:name field)) coercion-fns repetition-level definition-level)))
+                (recursively-stripe-record striped-record v field (conj parents (:name field)) (empty? record) coercion-fns repetition-level definition-level)))
             striped-record
             (schema/sub-fields schema))))
 
 (defmethod recursively-stripe-record :required
-  [striped-record record schema parents coercion-fns repetition-level definition-level]
-  #_(when (empty? record)
+  [striped-record record schema parents nil-parent? coercion-fns repetition-level definition-level]
+  (when (and (empty? record) (not nil-parent?))
     (throw (IllegalArgumentException. (format "Required field %s is missing" (format-parents parents)))))
   (reduce (fn [striped-record field]
             (let [v (get record (:name field))]
-              (recursively-stripe-record striped-record v field (conj parents (:name field)) coercion-fns repetition-level definition-level)))
+              (recursively-stripe-record striped-record v field (conj parents (:name field)) (empty? record) coercion-fns repetition-level definition-level)))
           striped-record
           (schema/sub-fields schema)))
 
 (defmethod recursively-stripe-record :seq
-  [striped-record record schema parents coercion-fns repetition-level definition-level]
+  [striped-record record schema parents nil-parent? coercion-fns repetition-level definition-level]
   (let [schema-without-repetition (assoc schema :repetition :optional)]
     (if (empty? record)
-      (recursively-stripe-record striped-record nil schema-without-repetition parents coercion-fns repetition-level definition-level)
+      (recursively-stripe-record striped-record nil schema-without-repetition parents true coercion-fns repetition-level definition-level)
       (reduce (fn [striped-record [sub-record repetition-level]]
-                (recursively-stripe-record striped-record sub-record schema-without-repetition parents coercion-fns repetition-level definition-level))
+                (recursively-stripe-record striped-record sub-record schema-without-repetition parents false coercion-fns repetition-level definition-level))
               striped-record
               (->> (interleave record (cons repetition-level (repeat (:repetition-level schema)))) (partition 2))))))
 
 (defmethod recursively-stripe-record :map
-  [striped-record record schema parents coercion-fns repetition-level definition-level]
+  [striped-record record schema parents nil-parent? coercion-fns repetition-level definition-level]
   (let [schema-as-list (assoc schema :repetition :list)]
     (if (empty? record)
-      (recursively-stripe-record striped-record nil schema-as-list parents coercion-fns repetition-level definition-level)
-      (recursively-stripe-record striped-record (map (fn [[k v]] {:key k :value v}) record) schema-as-list parents
+      (recursively-stripe-record striped-record nil schema-as-list parents true coercion-fns repetition-level definition-level)
+      (recursively-stripe-record striped-record (map (fn [[k v]] {:key k :value v}) record) schema-as-list parents false
                                  coercion-fns repetition-level definition-level))))
 
-(defn- stripe-record [record schema new-striped-record-fn coercion-fns]
-  (recursively-stripe-record (new-striped-record-fn) record schema [] coercion-fns 0 0))
+(defn- recursively-stripe-record* [record schema new-striped-record-fn coercion-fns]
+  (recursively-stripe-record (new-striped-record-fn) record schema [] false coercion-fns 0 0))
 
-(defn stripe-fn [schema]
+(defn- striper [schema]
   (let [value-types (schema/value-types schema)
         coercion-fns (coercion-fns-vec value-types)
         num-cols (count value-types)]
     (fn [record]
-      (stripe-record record schema #(new-striped-record num-cols) coercion-fns))))
+      (try
+        (recursively-stripe-record* record schema #(new-striped-record num-cols) coercion-fns)
+        (catch Exception e
+          (throw (IllegalArgumentException. (format "Failed to stripe record '%s'" record) e)))))))
+
+(defn stripe-record [record schema]
+  ((striper schema) record))
--- test/dendrite/striping_test.clj
+++ test/dendrite/striping_test.clj
@@ -5,39 +5,109 @@
             [dendrite.striping :refer :all]))
 
 (def dremel-paper-schema
-  {:docid (s/req 'int64)
-   :links {:forward ['in64]
-           :backward ['int64]}
-   :name [{:language [{:code (s/req 'string)
-                       :country 'string}]
-           :url 'string}]})
+  (-> {:docid (s/req 'int64)
+       :links {:forward ['in64]
+               :backward ['int64]}
+       :name [{:language [{:code (s/req 'string)
+                           :country 'string}]
+               :url 'string}]}
+      s/parse
+      s/annotate))
 
 (deftest dremel-paper
   (testing "Record striping matches dremel paper"
-    (let [schema (-> dremel-paper-schema s/parse s/annotate)]
-      (= (stripe-record {:docid 10
-                         :links {:forward [20 40 60]}
-                         :name [{:language [{:code "en-us" :country "us"}
-                                            {:code "en"}]
-                                 :url "http://A"}
-                                {:url "http://B"}
-                                {:language [{:code "en-gb" :country "gb"}]}]}
-                        schema)
-         [[(leveled-value 0 0 10)]
-          [(leveled-value 0 2 20) (leveled-value 1 2 40) (leveled-value 1 2 60)]
-          [(leveled-value 0 1 nil)]
-          [(leveled-value 0 2 "en-us") (leveled-value 2 2 "en") (leveled-value 1 1 nil)
-           (leveled-value 1 2 "en-gb")]
-          [(leveled-value 0 3 "us") (leveled-value 2 2 nil) (leveled-value 1 1 nil) (leveled-value 1 3 "gb")]
-          [(leveled-value 0 2 "http://A") (leveled-value 1 2 "http://B") (leveled-value 1 1 nil)]])
-      (= (stripe-record {:docid 20
-                         :links {:backward [10 30]
-                                 :forward [80]}
-                         :name [{:url "http://C"}]}
-                        schema)
-         [[(leveled-value 0 0 20)]
-          [(leveled-value 0 2 80)]
-          [(leveled-value 0 2 10) (leveled-value 1 2 30)]
-          [(leveled-value 0 1 nil)]
-          [(leveled-value 0 1 nil)]
-          [(leveled-value 0 2 "http://C")]]))))
+    (= (stripe-record {:docid 10
+                       :links {:forward [20 40 60]}
+                       :name [{:language [{:code "en-us" :country "us"}
+                                          {:code "en"}]
+                               :url "http://A"}
+                              {:url "http://B"}
+                              {:language [{:code "en-gb" :country "gb"}]}]}
+                      dremel-paper-schema)
+       [[(leveled-value 0 0 10)]
+        [(leveled-value 0 2 20) (leveled-value 1 2 40) (leveled-value 1 2 60)]
+        [(leveled-value 0 1 nil)]
+        [(leveled-value 0 2 "en-us") (leveled-value 2 2 "en") (leveled-value 1 1 nil)
+         (leveled-value 1 2 "en-gb")]
+        [(leveled-value 0 3 "us") (leveled-value 2 2 nil) (leveled-value 1 1 nil) (leveled-value 1 3 "gb")]
+        [(leveled-value 0 2 "http://A") (leveled-value 1 2 "http://B") (leveled-value 1 1 nil)]])
+    (= (stripe-record {:docid 20
+                       :links {:backward [10 30]
+                               :forward [80]}
+                       :name [{:url "http://C"}]}
+                      dremel-paper-schema)
+       [[(leveled-value 0 0 20)]
+        [(leveled-value 0 2 80)]
+        [(leveled-value 0 2 10) (leveled-value 1 2 30)]
+        [(leveled-value 0 1 nil)]
+        [(leveled-value 0 1 nil)]
+        [(leveled-value 0 2 "http://C")]])))
+
+(deftest invalid-records
+  (testing "Missing required field"
+    (let [schema (-> {:docid (s/req 'int64)
+                      :name [{:language {:country 'string
+                                         :code (s/req 'string)}}]}
+                     s/parse s/annotate)]
+      (is (stripe-record {:docid 10} schema))
+      (is (thrown? IllegalArgumentException (stripe-record {} schema)))
+      #_(is (thrown? IllegalArgumentException (stripe-record {} dremel-paper-schema)))
+      #_(is (thrown? IllegalArgumentException (stripe-record {}))))))
+
+(deftest invalid-records
+  (testing "Missing required field"
+    (let [schema (-> {:docid (s/req 'int64)
+                      :name [{:language (s/req {:country 'string
+                                                :code (s/req 'string)})}
+                             :url 'string]}
+                     s/parse s/annotate)]
+      (are [x] (stripe-record x schema)
+           {:docid 10}
+           {:docid 10 :name []}
+           {:docid 10 :name [{}]}
+           {:docid 10 :name [{:language {:code "en-us"}}]})
+      (are [x] (thrown? IllegalArgumentException (stripe-record x schema))
+           {}
+           {:docid 10 :name [{:url "http://A"}]}
+           (:docid 10 :name [{:language {}}])
+           {:docid 10 :name [{:language {:country "us"}}]})))
+  (testing "Incompatible value types"
+    (let [schema (-> {:boolean 'boolean
+                      :int 'int32
+                      :long 'int64
+                      :float 'float
+                      :double 'double
+                      :string 'string
+                      :fixed-length-byte-array 'fixed-length-byte-array
+                      :byte-array 'byte-array
+                      :char 'char
+                      :bigint 'bigint
+                      :keyword 'keyword
+                      :symbol 'symbol}
+                     s/parse s/annotate)]
+      (are [x] (stripe-record x schema)
+           {:boolean true}
+           {:boolean nil}
+           {:int 2}
+           {:long 2}
+           {:float 2}
+           {:double 2}
+           {:string "foo"}
+           {:fixed-length-byte-array (byte-array 2)}
+           {:byte-array (byte-array 2)}
+           {:string "foo"}
+           {:char \c}
+           {:bigint 2}
+           {:keyword :foo}
+           {:symbol 'foo})
+      (are [x] (thrown? IllegalArgumentException (stripe-record x schema))
+           {:int [1 2]}
+           {:int "2"}
+           {:long "2"}
+           {:float "2"}
+           {:double "2"}
+           {:fixed-length-byte-array [1 2 3]}
+           {:byte-array [1 2 3]}
+           {:char "f"}
+           {:bigint "foo"}
+           {:symbol 2}))))
