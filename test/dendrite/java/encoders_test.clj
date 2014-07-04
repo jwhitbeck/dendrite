f978e4eb2b2fa5a9042b9293baaaaed7b967598c
John Whitbeck <john@whitbeck.fr>
2014-07-03 18:26:24 -0700

    Refactor code duplication

--- test/dendrite/java/encoders_test.clj
+++ test/dendrite/java/encoders_test.clj
@@ -42,13 +42,15 @@
     (.writeTo encoder baw)
     (seq (.buffer baw))))
 
+(defn test-finish-idempotence [encoder-constructor input-seq]
+  (let [finish-fn (fn [n] (finish-repeatedly n encoder-constructor input-seq))]
+    (is (every? true? (map = (finish-fn 0) (finish-fn 3))))))
+
 (deftest boolean-encoders
   (testing "packed encoder/decoder"
     (test-encoder #(BooleanPackedEncoder.) #(BooleanPackedDecoder. %) (repeatedly helpers/rand-bool)))
   (testing "packed encoder's finish method is idempotent"
-    (let [rand-bools (repeatedly helpers/rand-bool)
-          finish-fn (fn [n] (finish-repeatedly n #(BooleanPackedEncoder.) rand-bools))]
-      (is (every? true? (map = (finish-fn 0) (finish-fn 3)))))))
+    (test-finish-idempotence #(BooleanPackedEncoder.) (repeatedly helpers/rand-bool))))
 
 (deftest int-encoders
   (testing "plain encoder/decoder"
@@ -64,9 +66,7 @@
                     #(IntFixedBitWidthPackedRunLengthDecoder. % 32)
                     (repeatedly helpers/rand-int))))
   (testing "fixed-bit-width packed run-length encoder's finish method is idempotent"
-    (let [rand-ints (repeatedly helpers/rand-int)
-          finish-fn (fn [n] (finish-repeatedly n #(IntFixedBitWidthPackedRunLengthEncoder. 32) rand-ints))]
-      (is (every? true? (map = (finish-fn 0) (finish-fn 3))))))
+    (test-finish-idempotence #(IntFixedBitWidthPackedRunLengthEncoder. 32) (repeatedly helpers/rand-int)))
   (testing "packed run-length encoder"
     (testing "sparse input"
       (test-encoder #(IntPackedRunLengthEncoder.)
@@ -77,17 +77,13 @@
                     #(IntPackedRunLengthDecoder. %)
                     (repeatedly helpers/rand-int))))
   (testing "packed run-length encoder's finish method is idempotent"
-    (let [rand-ints (repeatedly helpers/rand-int)
-          finish-fn (fn [n] (finish-repeatedly n #(IntPackedRunLengthEncoder.) rand-ints))]
-      (is (every? true? (map = (finish-fn 0) (finish-fn 3))))))
+    (test-finish-idempotence #(IntPackedRunLengthEncoder.) (repeatedly helpers/rand-int)))
   (testing "packed delta encoder/decoder"
     (test-encoder #(IntPackedDeltaEncoder.)
                   #(IntPackedDeltaDecoder. %)
                   (repeatedly helpers/rand-int)))
   (testing "packed delta encoder's finish method is idempotent"
-    (let [rand-ints (repeatedly helpers/rand-int)
-          finish-fn (fn [n] (finish-repeatedly n #(IntPackedDeltaEncoder.) rand-ints))]
-      (is (every? true? (map = (finish-fn 0) (finish-fn 3)))))))
+    (test-finish-idempotence #(IntPackedDeltaEncoder.) (repeatedly helpers/rand-int))))
 
 (deftest long-encoders
   (testing "plain encoder/decoder"
@@ -127,15 +123,11 @@
                                        #(ByteArrayDeltaLengthDecoder. %) rand-byte-arrays)]
       (is (every? true? (map helpers/array= read-byte-arrays rand-byte-arrays)))))
   (testing "delta-length encoder's finish method is idempotent"
-    (let [rand-byte-arrays (repeatedly helpers/rand-byte-array)
-          finish-fn (fn [n] (finish-repeatedly n #(ByteArrayDeltaLengthEncoder.) rand-byte-arrays))]
-      (is (every? true? (map = (finish-fn 0) (finish-fn 3))))))
+    (test-finish-idempotence #(ByteArrayDeltaLengthEncoder.) (repeatedly helpers/rand-byte-array)))
   (testing "incremental encoder/decoder"
     (let [rand-byte-arrays (repeatedly helpers/rand-byte-array)
           read-byte-arrays (write-read #(ByteArrayIncrementalEncoder.)
                                        #(ByteArrayIncrementalDecoder. %) rand-byte-arrays)]
       (is (every? true? (map helpers/array= read-byte-arrays rand-byte-arrays)))))
   (testing "incremental encoder's finish method is idempotent"
-    (let [rand-byte-arrays (repeatedly helpers/rand-byte-array)
-          finish-fn (fn [n] (finish-repeatedly n #(ByteArrayIncrementalEncoder.) rand-byte-arrays))]
-      (is (every? true? (map = (finish-fn 0) (finish-fn 3)))))))
+    (test-finish-idempotence #(ByteArrayIncrementalEncoder.) (repeatedly helpers/rand-byte-array))))
