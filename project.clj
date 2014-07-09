22e9cbcee4e74fc28c45b1d05c8e2944fa4b7b2f
John Whitbeck <john@whitbeck.fr>
2014-07-08 19:07:05 -0700

    Upgrade clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [net.jpountz.lz4/lz4 "1.2.0"]
                  [org.clojure/data.fressian "0.2.0"]]
