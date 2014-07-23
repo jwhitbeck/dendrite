cd885f308d2227f901616d9357c2d4ba82804819
John Whitbeck <john@whitbeck.fr>
2014-07-23 09:41:57 -0700

    Expand public API

--- src/dendrite.clj
+++ src/dendrite.clj
@@ -7,6 +7,9 @@
 (defalias byte-buffer-writer core/byte-buffer-writer)
 (defalias byte-buffer-reader core/byte-buffer-reader)
 
+(defalias file-writer core/file-writer)
+(defalias file-reader core/file-reader)
+
 (defalias write! core/write!)
 (defalias set-metadata! core/set-metadata!)
 (defalias byte-buffer! core/byte-buffer!)
@@ -16,6 +19,7 @@
 (defalias metadata core/metadata)
 (defalias schema core/schema)
 
+(defalias read-schema-string s/read-string)
 (defalias col s/col)
 (defalias req s/req)
 (defalias tag s/tag)
