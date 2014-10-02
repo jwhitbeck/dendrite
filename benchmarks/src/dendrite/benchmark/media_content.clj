052d32a18642f164bdf79a9354995d88a7545ada
John Whitbeck <john@whitbeck.fr>
2014-10-01 22:06:06 -0700

    Fix benchmark #2 data

--- benchmarks/src/dendrite/benchmark/media_content.clj
+++ benchmarks/src/dendrite/benchmark/media_content.clj
@@ -8,14 +8,14 @@
    {:name "images.title" :type "Sentences" :min 1 :max 1}
    {:name "images.width" :type "Custom List" :values [240 468 88 300 728 234 250 125 180 200 320 120 160 336]}
    {:name "images.height" :type "Custom List" :values [240 60 50 31 600 90 150 100 250 280 125 200 400]}
-   {:name "images.size" :type "Number" :min (* 1024) :max (* 100 1024) :decimals 0}
+   {:name "images.size" :type "Custom List" :values ["SMALL" "LARGE"]}
    {:name "media.uri" :type "URL" :includeQueryString false}
    {:name "media.title" :type "Sentences" :min 1 :max 1}
    {:name "media.width" :type "Custom List" :values [480 720 1280 1920 3840 7680]}
    {:name "media.height" :type "Custom List" :values [480 576 720 1080 2160 4320]}
    {:name "media.format" :type "Custom List" :values ["MP4" "MKV" "AVI" "MOV"]}
    {:name "media.duration" :type "Number" :min 1 :max 3600 :decimals 0}
-   {:name "media.size" :type "Custom List" :values ["SMALL" "LARGE"]}
+   {:name "media.size" :type "Number" :min (* 1024) :max (* 100 1024) :decimals 0}
    {:name "media.bitrate" :type "Custom List" :values [400 700 1500 2500 4000]}
    {:name "media.persons" :type "JSON Array" :maxItems 3}
    {:name "media.persons.item" :type "Full Name" :values ["FLASH" "JAVA"]}
@@ -56,20 +56,24 @@
                               {:name "height" :type "int"}
                               {:name "copyright" :type "string"}
                               {:name "duration" :type "int"}
-                              {:name "size" :type "string"}
+                              {:name "size" :type "int"}
                               {:name "title" :type "string"}
                               {:name "persons" :type {:name "persons-list"
                                                       :type "array"
                                                       :items {:type "string"}}}
                               {:name "bitrate" :type "int"}
-                              {:name "player" :type "string"}
+                              {:name "player" :type {:name "playerEnum"
+                                                     :type "enum"
+                                                     :symbols ["JAVA" "FLASH"]}}
                               {:name "uri" :type "string"}]}}
              {:name "images"
               :type {:name "images-list"
                      :type "array"
                      :items {:name "image"
                              :type "record"
-                             :fields [{:name "size" :type "int"}
+                             :fields [{:name "size" :type {:name "imageSize"
+                                                           :type "enum"
+                                                           :symbols ["SMALL" "LARGE"]}}
                                       {:name "height" :type "int"}
                                       {:name "width" :type "int"}
                                       {:name "title" :type "string"}
