3f324765580175792b381270b0df1bfc27526515
John Whitbeck <john@whitbeck.fr>
2015-02-07 15:00:41 -0800

    Update copyright notices

--- dendrite/src/dendrite/metadata.clj
+++ dendrite/src/dendrite/metadata.clj
@@ -1,4 +1,4 @@
-;; Copyright (c) 2013-2014 John Whitbeck. All rights reserved.
+;; Copyright (c) 2013-2015 John Whitbeck. All rights reserved.
 ;;
 ;; The use and distribution terms for this software are covered by the
 ;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
