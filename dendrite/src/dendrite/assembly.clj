7bff5c9e605821f21448834a32c923d9a19c3b48
John Whitbeck <john@whitbeck.fr>
2015-03-29 19:12:45 -0700

    Perform record assembly in Java

--- dendrite/src/dendrite/assembly.clj
+++ dendrite/src/dendrite/assembly.clj
@@ -13,7 +13,7 @@
             [dendrite.schema :as schema]
             [dendrite.utils :as utils])
   (:import [clojure.lang ArraySeq]
-           [dendrite.java LeveledValue PersistentFixedKeysHashMap]))
+           [dendrite.java Assembly LeveledValue]))
 
 (set! *warn-on-reflection* true)
 
@@ -41,14 +41,8 @@
   [field]
   (let [col-idx (-> field :column-spec :query-column-index)]
     (if (-> field :column-spec :max-repetition-level pos?)
-      (fn [^objects leveled-values-array]
-        (let [lvls (aget leveled-values-array col-idx)
-              ^LeveledValue lv (first lvls)]
-          (aset leveled-values-array col-idx (rest lvls))
-          (when lv
-            (.value lv))))
-      (fn [^objects leveled-values-array]
-        (aget leveled-values-array col-idx)))))
+      (Assembly/getNonRepeatedValueFn col-idx)
+      (Assembly/getRequiredNonRepeatedValueFn col-idx))))
 
 (defmethod assemble-fn* :repeated-value
   [field]
@@ -58,52 +52,18 @@
         max-dl (:max-definition-level cs)
         rep-type (:repetition field)
         empty-coll (if (= :set rep-type) #{} [])
-        ass-fn (fn [^objects leveled-values-array]
-                 (let [lvs (aget leveled-values-array col-idx)
-                       ^LeveledValue flv (first lvs)
-                       next-rl (if-let [^LeveledValue n (second lvs)] (.repetitionLevel n) 0)]
-                   (if (and (nil? (.value flv)) (> max-dl (.definitionLevel flv)) (> max-rl next-rl))
-                     (do (aset leveled-values-array col-idx (rest lvs))
-                         nil)
-                     (let [ret (loop [rlvs (rest lvs)
-                                      tr (conj! (transient empty-coll) (.value flv))]
-                                 (let [^LeveledValue lv (first rlvs)]
-                                   (if (and lv (= max-rl (.repetitionLevel lv)))
-                                     (recur (rest rlvs) (conj! tr (.value lv)))
-                                     (do (aset leveled-values-array col-idx rlvs)
-                                         (persistent! tr)))))]
-                       (when-not (empty? ret)
-                         ret)))))
+        ass-fn (Assembly/getRepeatedValueFn col-idx max-rl max-dl empty-coll)
         reader-fn (:reader-fn field)]
     (cond->> ass-fn
              (= rep-type :list) (comp seq)
              reader-fn (comp-some reader-fn))))
 
-(def ^:private undefined PersistentFixedKeysHashMap/UNDEFINED)
-
-(defn- record-ctor-fn [field-names field-ass-fns]
-  (let [fact (PersistentFixedKeysHashMap/factory field-names)
-        ^objects field-ass-fn-array (into-array clojure.lang.IFn field-ass-fns)
-        n (count field-names)]
-    (fn [^objects leveled-values-array]
-      (let [vals (object-array n)]
-        (loop [i 0]
-          (if (= i n)
-            (.create fact vals)
-            (let [v ((aget field-ass-fn-array i) leveled-values-array)]
-              (if (nil? v)
-                (aset vals i undefined)
-                (aset vals i v))
-              (recur (inc i)))))))))
-
 (defmethod assemble-fn* :non-repeated-record
   [field]
   (let [name-fn-map (reduce (fn [m fld] (assoc m (:name fld) (assemble-fn* fld))) {} (:sub-fields field))
-        record-ctor (record-ctor-fn (map :name (:sub-fields field)) (map assemble-fn* (:sub-fields field)))
-        ass-fn (fn [^objects leveled-values-array]
-                 (let [rec (record-ctor leveled-values-array)]
-                   (when-not (empty? rec)
-                     rec)))]
+        record-ctor (Assembly/getRecordConstructorFn (map :name (:sub-fields field))
+                                                     (map assemble-fn* (:sub-fields field)))
+        ass-fn (Assembly/getNonRepeatedRecordFn record-ctor)]
     (if-let [reader-fn (:reader-fn field)]
       (comp-some reader-fn ass-fn)
       ass-fn)))
@@ -111,29 +71,12 @@
 (defmethod assemble-fn* :repeated-record
   [field]
   (let [next-rl-col-idx (-> field schema/column-specs last :query-column-index)
-        next-rl-fn (fn [^objects lva] (if-let [^LeveledValue lv (first (aget lva next-rl-col-idx))]
-                                        (.repetitionLevel lv)
-                                        0))
-        next-dl-fn (fn [^objects lva] (if-let [^LeveledValue lv (first (aget lva next-rl-col-idx))]
-                                        (.definitionLevel lv)
-                                        0))
         rep-lvl (:repetition-level field)
         def-lvl (:definition-level field)
         rep-type (:repetition field)
         non-repeated-ass-fn (assemble-fn* (assoc field :repetition :optional :reader-fn nil))
         empty-coll (if (= :set rep-type) #{} [])
-        ass-fn (fn [^objects leveled-values-array]
-                 (let [dl (next-dl-fn leveled-values-array)
-                       fr (non-repeated-ass-fn leveled-values-array)]
-                   (when-not (and (nil? fr) (> def-lvl dl))
-                     (let [ret (loop [trvs (conj! (transient empty-coll) fr)
-                                      nrl (next-rl-fn leveled-values-array)]
-                                 (if (> rep-lvl nrl)
-                                   (persistent! trvs)
-                                   (let [nr (non-repeated-ass-fn leveled-values-array)]
-                                     (recur (conj! trvs nr) (next-rl-fn leveled-values-array)))))]
-                       (when-not (empty? ret)
-                         ret)))))
+        ass-fn (Assembly/getRepeatedRecordFn non-repeated-ass-fn next-rl-col-idx rep-lvl def-lvl empty-coll)
         reader-fn (:reader-fn field)]
     (cond->> ass-fn
              (= :list rep-type) (comp-some seq)
