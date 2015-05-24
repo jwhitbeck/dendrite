f0ed94cab5d4e1fcaff8f92b1b9aa4b5349bb4cf
John Whitbeck <john@whitbeck.fr>
2015-05-24 09:49:44 -0700

    Add core docstrings

--- dendrite/src/dendrite/core2.clj
+++ dendrite/src/dendrite/core2.clj
@@ -82,11 +82,34 @@
   (Schema/readString s))
 
 (defn reader
+  "Returns a dendrite reader for the provided file.
+
+  If provided, the options map supports the following keys:
+  :custom-types  - a map of of custom-type symbol to custom-type specification. Default: nil. See README for
+                   full explanation."
   (^dendrite.java.Reader [file] (reader nil file))
   (^dendrite.java.Reader [opts file]
     (Reader/create (Options/getReaderOptions opts) (io/as-file file))))
 
 (defn writer
+  "Returns a dendrite writer that outputs to a file according to the provided schema.
+
+  If provided, the options map supports the following keys:
+  :data-page-length        - the length in bytes of the data pages (default 262144)
+  :record-group-length     - the length in bytes of each record group (default 134217728)
+  :optimize-columns?       - either :all, :none or :default. If :all, will attempt to optimize the
+                             encoding and compression for each column; if :default, will only optimize
+                             columns with the default encoding & compression (i.e., plain/none); if :none,
+                             disables all optimization.
+  :compression-thresholds  - a map of compression method (e.g., deflate) to the minimum compression ratio
+                             (e.g., 2) below which the overhead of compression is not not deemed worthwhile.
+                             Default: {'deflate 1.5}
+  :invalid-input-handler   - a function with two arguments: record and exception. If an input record does
+                             not conform to the schema, it will be passed to this function along with the
+                             exception it triggered. By default, this option is nil and exceptions
+                             triggered by invalid records are not caught.
+  :custom-types            - a map of of custom-type symbol to custom-type specification. See README for
+                             full explanation."
   (^dendrite.java.Writer [schema file] (writer nil schema file))
   (^dendrite.java.Writer [opts schema file]
     (Writer/create (Options/getWriterOptions opts) schema (io/as-file file))))
@@ -102,23 +125,45 @@
     (.fold this n combinef reducef)))
 
 (defn read
+  "Returns a view of all the records in the reader. This view is Seqable (lazy), reducible, and foldable (per
+  clojure.core.reducers, in which case the folding is done as part of record assembly).
+
+  If provided, the options map supports the following keys:
+  :missing-fields-as-nil? - should be true (default) or false. If true, then fields that are specified in the
+                            query but are not present in this reader's schema will be read as nil values. If
+                            false, querying for fields not present in the schema will throw an exception.
+  :query                  - the query. Default: '_. See README for full explanation.
+  :entrypoint             - a sequence of keys to begin the query within a subset of the schema. Cannot
+                            contain any keys to repeated fields. See README for full explanation.
+  :readers                - a map of query tag symbol to tag function. Default: nil. See README for full
+                            explanation."
   ([^Reader reader] (read nil reader))
   ([opts ^Reader reader] (.read reader (Options/getReadOptions opts))))
 
 (defn pmap
+  "Returns a view of all the records in the reader with the provided function applied to them. This is a
+  convenience function that is roughly equivalent to (read {:query (tag 'foo '_) :readers {'foo f}} reader).
+  See read for full details on available options."
   ([f ^Reader reader] (pmap nil f reader))
   ([opts f ^Reader reader] (.pmap reader (Options/getReadOptions opts) f)))
 
 (defn stats
+  "Returns a map containing all the stats associated with this reader. The tree top-level keys
+  are :global, :record-groups, and :columns, that, respectively, contain stats summed over the entire file,
+  summed across all column-chunks in the same record-groups, and summed across all column-chunks belonging
+  to the same column."
   [^Reader reader] (.stats reader))
 
 (defn schema
+  "Returns this reader's schema."
   [^Reader reader] (.schema reader))
 
 (defn set-metadata!
+  "Sets the user-defined metadata for this writer."
   [^Writer writer metadata]
   (.setMetadata writer (-> metadata pr-str Types/toByteArray ByteBuffer/wrap)))
 
 (defn metadata
+  "Returns the user-defined metadata for this reader."
   [^Reader reader]
   (-> (.getMetadata reader) Types/toByteArray Types/toString edn/read-string))
