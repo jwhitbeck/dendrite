6ecddadf71bdce59ea5a8f2d22d4832af9d46c23
John Whitbeck <john@whitbeck.fr>
2015-08-28 01:16:33 -0700

    Upgrade benchmark project dependencies

--- benchmarks/project.clj
+++ benchmarks/project.clj
@@ -7,15 +7,15 @@
   :main dendrite.benchmarks.core
   :plugins [[lein-protobuf "0.4.1"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cheshire "5.4.0"]
+                 [cheshire "5.5.0"]
                  [com.damballa/abracad "0.4.12"]
                  [com.google.protobuf/protobuf-java "2.6.1"]
-                 [com.taoensso/nippy "2.8.0"]
+                 [com.taoensso/nippy "2.9.0"]
                  [dendrite "0.4.4"]
-                 [org.clojure/tools.cli "0.3.1"]
+                 [org.clojure/tools.cli "0.3.3"]
                  [org.flatland/protobuf "0.8.1"]
                  [prismatic/plumbing "0.4.4"]
                  [net.jpountz.lz4/lz4 "1.3.0"]
-                 [clj-http "1.1.2"]
+                 [clj-http "2.0.0"]
                  [ring/ring-codec "1.0.0"]
-                 [org.clojure/data.fressian "0.2.0"]])
+                 [org.clojure/data.fressian "0.2.1"]])
