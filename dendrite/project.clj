10ab1d2b2a29ee672896fe2e0028b368b2450871
John Whitbeck <john@whitbeck.net>
2017-10-03 14:40:25 +0200

    Version 0.5.6

--- dendrite/project.clj
+++ dendrite/project.clj
@@ -1,4 +1,4 @@
-(defproject dendrite "0.5.6-SNAPSHOT"
+(defproject dendrite "0.5.6"
   :description "A Dremel-like columnar storage format for Clojure."
   :url "https://github.com/jwhitbeck/dendrite"
   :license {:name "Eclipse Public License"
